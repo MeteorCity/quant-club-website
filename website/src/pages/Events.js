cb94287d4a42e45452c1ebb9b04e933a081fbc37
John Talghader <johntalghader@Johns-Laptop.local>
2024-09-14 14:33:37 -0500

    small style change

--- website/src/pages/Events.js
+++ website/src/pages/Events.js
@@ -5,6 +5,12 @@ function Events() {
   return (
     <div className="events-page">
       <Header />
+      <main>
+        <h1 className="events-txt-h1">Upcoming Events</h1>
+        <div className="events-box">
+
+        </div>
+      </main>
     </div>
   )
 }
