5eda2a84b6b32f3591aa049ee3ee0c3a34190de4
John Talghader <johntalghader@Johns-Laptop.local>
2024-09-14 12:15:54 -0500

    finish header and extract to component

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="logo">RQFC</div>
-        <h4>Events</h4>
-        <h4>Work with Us</h4>
-        <h4>Join</h4>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ website/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div className="logo">RQFC</div>
+        <h3>Events</h3>
+        <h3>Work with Us</h3>
+        <h3>Join</h3>
+    </header>
+  )
+}
+
+export default Header
